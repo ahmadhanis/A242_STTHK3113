a146fefd539fb6b4a974dfe220ddf52a81f5ae41
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2025-04-27 10:13:22 +0800

    Update oled_example.ino

--- oled_example/oled_example.ino
+++ oled_example/oled_example.ino
@@ -3,20 +3,22 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
 #define OLED_RESET -1
+#define I2C_SDA 4
+#define I2C_SCL 15
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
+  Wire.begin(I2C_SDA, I2C_SCL);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;)
       ;  // Don't proceed, loop forever
   }
   Serial.println("OLED INIT...");
-  setDisplay("Hello", "My Name is", "Hanis");
+  setDisplay("Welcome to Universiti", "Utara Malaysia", "Hello");
 }
 
 void setDisplay(String a, String b, String c) {
